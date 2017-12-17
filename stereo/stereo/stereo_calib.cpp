3e0d077b7f03831ce1ffb7e89e4b3a8297f9d0c1
Wan Youwen <1756429307@qq.com>
2017-12-17 22:14:39 +0800

    双目校准

--- stereo/stereo/stereo_calib.cpp
+++ stereo/stereo/stereo_calib.cpp
@@ -1,10 +1,46 @@
 /* This is sample from the OpenCV book. The copyright notice is below
  * 双目相机 矫正 
- * 用法 ./stereo_calib -w=6 -h=8  -s=2.43 stereo_calib.xml   我的
- * ./stereo_calib -w=9 -h=6 ../date/std/stereo_calib.xml 标准图像
- *  实际的正方格尺寸在程序中指定 const float squareSize = 2.43f;    2.43cm
+ * 用法 ./Stereo_Calibr -w=6 -h=8  -s=24.3 stereo_calib.xml   我的  ./Stereo_Calibr -w=8 -h=10 -s=200 stereo_calib.xml
+ * ./stereo_calib -w=9 -h=6 stereo_calib.xml 标准图像
+ *  实际的正方格尺寸在程序中指定 const float squareSize = 2.43f;    2.43cm  mm为单位的话为 24.3  0.1mm为精度的话为   243 注意 标定结果单位(纲量)和此一致
  * https://www.cnblogs.com/polly333/p/5013505.html 原理分析
  * http://blog.csdn.net/zc850463390zc/article/details/48975263
+ * 
+ * 径向畸变矫正 光学透镜特效  凸起                k1 k2 k3 三个参数确定
+ * Xp=Xd(1 + k1*r^2 + k2*r^4 + k3*r^6)
+ * Yp=Yd(1 + k1*r^2 + k2*r^4 + k3*r^6)
+ * 切向畸变矫正 装配误差                                 p1  p2  两个参数确定
+ * Xp=Xd + ( 2 * p1 * y  +p2 * (r^2 +2 * x^2) )
+ * Yp=Yd + ( p1 * (r^2 + 2 * y^2) + 2 * p2 * x )
+ * r^2 = x^2+y^2                  						  | Xw|
+ * 			| u|     |fx  0   ux 0|     |    R         T|     | Yw|
+ *       		| v| =  |0   fy  uy 0|  *  |  		   | *  | Zw|=M*W
+ * 			|1|      |0   0   1   0|      |    0 0  0 1|     | 1   |
+ * http://wiki.opencv.org.cn/index.php/Cv相机标定和三维重建
+ *   像素坐标齐次表示(3*1)  =  内参数矩阵 齐次表示 3*4  ×  外参数矩阵齐次表示 4*4 ×  物体世界坐标 齐次表示  4*1
+ *   内参数齐次 × 外参数齐次 整合 得到 投影矩阵  3*4    左右两个相机 投影矩阵 P1   P2
+ *  Q为 视差转深度矩阵 disparity-to-depth mapping matrix 
+ * 
+ *  http://blog.csdn.net/angle_cal/article/details/50800775
+ * Q= | 1   0    0  	  -C_x |
+ *  	  | 0   1    0  	  -C_y |
+ *   	  | 0   0    0         f    |
+ *   	  | 1   0    -1/T   (c_x-c_x')/T |
+ *     
+ *  以左相机光心为世界坐标系原点   左手坐标系Z  垂直向后指向 相机平面  
+ *           |x|      | x-C_x                   |     |X|
+ *           |y|      | y-C_y                   |     |Y|
+ *   Q  * |d| =   |f                            |  = |Z|======>  Z  =   Z/W =    -  f*T/(d-c_x+c_x')
+ *           |1|      |(-d+c_x-c_x')/T     |     |W|
+ * Z = f * T / D    f 焦距 量纲为像素点  T 左右相机基线长度 量纲和标定时 所给标定板尺寸 相同  D视差 量纲也为 像素点 分子分母约去，Z的量纲同 T
+ * 
+ * 
+ * CCD的尺寸是8mm X 6mm，帧画面的分辨率设置为640X480，那么毫米与像素点之间的转换关系就是80pixel/mm
+ * CCD传感器每个像素点的物理大小为dx*dy，相应地，就有 dx=dy=1/80
+ * 假设像素点的大小为k x l，其中 fx = f / k， fy = f / (l * sinA)， A一般假设为 90°，是指摄像头坐标系的偏斜度（就是镜头坐标和CCD是否垂直）。
+ * 摄像头矩阵（内参）的目的是把图像的点从图像坐标转换成实际物理的三维坐标。因此其中的fx,y, cx, cy 都是使用类似上面的纲量。
+ * 同样，Q 中的变量 f，cx, cy 也应该是一样的。
+ * 
    ************************************************** */
 
 #include "opencv2/calib3d/calib3d.hpp"
@@ -238,12 +274,13 @@ StereoCalib(const vector<string>& imagelist, Size boardSize, float squareSize_, 
                   imageSize, R, T, R1, R2, P1, P2, Q,
                   CALIB_ZERO_DISPARITY, 1, imageSize, &validRoi[0], &validRoi[1]);
  /* 
+  * R T 左相机到右相机 的 旋转 平移矩阵  R 3*3      T  3*1  T中第一个Tx为 基线长度 
     立体校正的时候需要两幅图像共面并且行对准 以使得立体匹配更加的可靠 
     使得两幅图像共面的方法就是把两个摄像头的图像投影到一个公共成像面上，这样每幅图像从本图像平面投影到公共图像平面都需要一个旋转矩阵R 
     stereoRectify 这个函数计算的就是从图像平面投影都公共成像平面的旋转矩阵Rl,Rr。 Rl,Rr即为左右相机平面行对准的校正旋转矩阵。 
     左相机经过Rl旋转，右相机经过Rr旋转之后，两幅图像就已经共面并且行对准了。 
     其中Pl,Pr为两个相机的投影矩阵，其作用是将3D点的坐标转换到图像的2D点的坐标:P*[X Y Z 1]' =[x y w]  
-    Q矩阵为重投影矩阵，即矩阵Q可以把2维平面(图像平面)上的点投影到3维空间的点:Q*[x y d 1] = [X Y Z W]。其中d为左右两幅图像的时差 
+    Q矩阵为重投影矩阵，即矩阵Q可以把2维平面(图像平面)上的点投影到3维空间的点:Q*[x y d 1] = [X Y Z W]。其中d为左右两幅图像的视差 
     */ 
     fs.open("extrinsics.yml", FileStorage::WRITE);
     if( fs.isOpened() )
