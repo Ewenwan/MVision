88ddb851a8ddba09b05251df08b6a420e18575d6
Wan Youwen <1756429307@qq.com>
2017-12-22 23:42:34 +0800

    Pangolin是一个用于OpenGL显示/交互以及视频输入

--- vSLAM/ch3/visualizeGeometry/visualizeGeometry.cpp
+++ vSLAM/ch3/visualizeGeometry/visualizeGeometry.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <iomanip>
+//命名空间
 using namespace std;
-
+//eigen核心库
 #include <Eigen/Core>
+//eigen几何学库
 #include <Eigen/Geometry>
+//使用eigen名字空间
 using namespace Eigen;
-
+//OpenGL输入/输出和视频显示的库
 #include <pangolin/pangolin.h>
 
+//结构体   旋转向量
 struct RotationMatrix
 {
-    Matrix3d matrix = Matrix3d::Identity();
+    Matrix3d matrix = Matrix3d::Identity();// 3*3 单位矩阵
 };
-
+// 旋转矩阵 输出 操作符 重载  ostream& 输出类型
 ostream& operator << ( ostream& out, const RotationMatrix& r ) 
 {
     out.setf(ios::fixed);
@@ -23,40 +27,40 @@ ostream& operator << ( ostream& out, const RotationMatrix& r )
     << "["<<matrix(2,0)<<","<<matrix(2,1)<<","<<matrix(2,2)<<"]";
     return out;
 }
-
+//旋转矩阵 输入操作符重载
 istream& operator >> (istream& in, RotationMatrix& r )
 {
     return in;
 }
-
+// 结构体  平移向量   
 struct TranslationVector
 {
     Vector3d trans = Vector3d(0,0,0);
 };
-
+//平移向量 输出操作符重载 
 ostream& operator << (ostream& out, const TranslationVector& t)
 {
     out<<"=["<<t.trans(0)<<','<<t.trans(1)<<','<<t.trans(2)<<"]";
     return out;
 }
-
+//平移向量 输入操作符重载 
 istream& operator >> ( istream& in, TranslationVector& t)
 {
     return in;
 }
-
+// 四元素 结构体
 struct QuaternionDraw
 {
     Quaterniond q;
 };
-
+//四元素 输出操作符重载 
 ostream& operator << (ostream& out, const QuaternionDraw quat )
 {
     auto c = quat.q.coeffs();
     out<<"=["<<c[0]<<","<<c[1]<<","<<c[2]<<","<<c[3]<<"]";
     return out;
 }
-
+//四元素  输入操作符重载 
 istream& operator >> (istream& in, const QuaternionDraw quat)
 {
     return in;
@@ -64,10 +68,10 @@ istream& operator >> (istream& in, const QuaternionDraw quat)
 
 int main ( int argc, char** argv )
 {
-    pangolin::CreateWindowAndBind ( "visualize geometry", 1000, 600 );
+    pangolin::CreateWindowAndBind ( "visualize geometry", 1000, 600 );//窗口
     glEnable ( GL_DEPTH_TEST );
     pangolin::OpenGlRenderState s_cam (
-        pangolin::ProjectionMatrix ( 1000, 600, 420, 420, 500, 300, 0.1, 1000 ),
+        pangolin::ProjectionMatrix ( 1000, 600, 420, 420, 500, 300, 0.1, 1000 ),//摄影矩阵  8自由度
         pangolin::ModelViewLookAt ( 3,3,3,0,0,0,pangolin::AxisY )
     );
     
@@ -75,7 +79,7 @@ int main ( int argc, char** argv )
     
     pangolin::View& d_cam = pangolin::CreateDisplay().SetBounds(0.0, 1.0, pangolin::Attach::Pix(UI_WIDTH), 1.0, -1000.0f/600.0f).SetHandler(new pangolin::Handler3D(s_cam));
     
-    // ui
+    // 可视化界面显示 ui
     pangolin::Var<RotationMatrix> rotation_matrix("ui.R", RotationMatrix());
     pangolin::Var<TranslationVector> translation_vector("ui.t", TranslationVector());
     pangolin::Var<TranslationVector> euler_angles("ui.rpy", TranslationVector());
@@ -91,28 +95,28 @@ int main ( int argc, char** argv )
         
         pangolin::OpenGlMatrix matrix = s_cam.GetModelViewMatrix();
         Matrix<double,4,4> m = matrix;
-        // m = m.inverse();
+        // m = m.inverse();  旋转矩阵
         RotationMatrix R; 
         for (int i=0; i<3; i++)
             for (int j=0; j<3; j++)
                 R.matrix(i,j) = m(j,i);
         rotation_matrix = R;
-        
+        //平移向量
         TranslationVector t;
         t.trans = Vector3d(m(0,3), m(1,3), m(2,3));
         t.trans = -R.matrix*t.trans;
         translation_vector = t;
-        
+        //欧拉角
         TranslationVector euler;
         euler.trans = R.matrix.transpose().eulerAngles(2,1,0);
         euler_angles = euler;
-        
+        //四元素
         QuaternionDraw quat;
         quat.q = Quaterniond(R.matrix);
         quaternion = quat;
-        
+        //颜色
         glColor3f(1.0,1.0,1.0);
-        
+        //画立方体
         pangolin::glDrawColouredCube();
         // draw the original axis 
         glLineWidth(3);
