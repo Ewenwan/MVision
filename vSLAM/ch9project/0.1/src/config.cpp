2c8d4a03ea6dbdfdd33816c4fd55e15ff3f1269b
Wan Youwen <1756429307@qq.com>
2018-01-10 21:53:30 +0800

    myslam::Config 读取 配置文件类

--- vSLAM/ch9project/0.1/src/config.cpp
+++ vSLAM/ch9project/0.1/src/config.cpp
@@ -1,46 +1,30 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2016  <copyright holder> <email>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *myslam::Config 配置文件
  */
 
 #include "myslam/config.h"
 
 namespace myslam //命令空间下 防止定义的出其他库里的同名函数
 {
- //   读取参数文件
-void Config::setParameterFile( const std::string& filename )
-{
-    if ( config_ == nullptr )
-        config_ = shared_ptr<Config>(new Config);
-    config_->file_ = cv::FileStorage( filename.c_str(), cv::FileStorage::READ );//读文件
-    if ( config_->file_.isOpened() == false )//打开文件错误
+    //   读取参数文件
+    void Config::setParameterFile( const std::string& filename )
     {
-        std::cerr<<"parameter file "<<filename<<" does not exist."<<std::endl;//文件不存在
-        config_->file_.release();//释放
-        return;
+	if ( config_ == nullptr )//为空
+	    config_ = shared_ptr<Config>(new Config);//初始化一个
+	config_->file_ = cv::FileStorage( filename.c_str(), cv::FileStorage::READ );//读文件
+	if ( config_->file_.isOpened() == false )//打开文件错误
+	{
+	    std::cerr<<"参数文件 parameter file "<<filename<<" 文件不存在 "<<std::endl;//文件不存在
+	    config_->file_.release();//释放
+	    return;
+	}
     }
-}
-//析构函数 退出函数
-Config::~Config()
-{
-    if ( file_.isOpened() )
-        file_.release();
-}
-
-shared_ptr<Config> Config::config_ = nullptr;
+    //析构函数 退出函数  关闭文件
+    Config::~Config()
+    {
+	if ( file_.isOpened() )// 若文件打开，
+	    file_.release();//若文件打开，则 退出时 关闭文件
+    }
+    shared_ptr<Config> Config::config_ = nullptr;//初始化为 空
 
 }
