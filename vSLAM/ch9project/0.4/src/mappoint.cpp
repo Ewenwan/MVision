04dd84d348f408b4783389253bd733e179b8a4b4
Wan Youwen <1756429307@qq.com>
2018-01-15 22:00:23 +0800

    地图点包含 三维点 姿态 帧 对应二维图像像素点的描述子

--- vSLAM/ch9project/0.4/src/mappoint.cpp
+++ vSLAM/ch9project/0.4/src/mappoint.cpp
@@ -1,58 +1,53 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2016  <copyright holder> <email>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * 地图点类  加入 所处帧  对赢二维图像点的特征描述子 用于 匹配
  */
 
+#ifndef MAPPOINT_H
+#define MAPPOINT_H
+
 #include "myslam/common_include.h"
-#include "myslam/mappoint.h"
 
 namespace myslam
 {
-
-MapPoint::MapPoint()
-: id_(-1), pos_(Vector3d(0,0,0)), norm_(Vector3d(0,0,0)), good_(true), visible_times_(0), matched_times_(0)
+    
+class Frame;
+class MapPoint
 {
-
-}
-
-MapPoint::MapPoint ( long unsigned int id, const Vector3d& position, const Vector3d& norm, Frame* frame, const Mat& descriptor )
-: id_(id), pos_(position), norm_(norm), good_(true), visible_times_(1), matched_times_(1), descriptor_(descriptor)
-{
-    observed_frames_.push_back(frame);
-}
-
-MapPoint::Ptr MapPoint::createMapPoint()
-{
-    return MapPoint::Ptr( 
-        new MapPoint( factory_id_++, Vector3d(0,0,0), Vector3d(0,0,0) )
+public:
+    typedef shared_ptr<MapPoint> Ptr;
+    unsigned long      id_;        // ID
+    static unsigned long factory_id_;    // factory id
+    bool        good_;      // wheter a good point 
+    Vector3d    pos_;       // Position in world
+    Vector3d    norm_;      // Normal of viewing direction 
+    Mat         descriptor_; // Descriptor for matching 
+    
+    list<Frame*>    observed_frames_;   // key-frames that can observe this point 
+    
+    int         matched_times_;     // being an inliner in pose estimation
+    int         visible_times_;     // being visible in current frame 
+    
+    MapPoint();
+    MapPoint( 
+        unsigned long id, 
+        const Vector3d& position, //位置
+        const Vector3d& norm, //方位
+        Frame* frame=nullptr, //所属帧
+        const Mat& descriptor=Mat() //加入特征描述子
     );
+   
+   //  inline 标识  常在 较短的函数前面  表示 在执行该函数时 不用新开辟内存空间  之间类似 宏定义 一样替换 节省时间
+    inline cv::Point3f getPositionCV() const {
+        return cv::Point3f( pos_(0,0), pos_(1,0), pos_(2,0) );//返回 opencv格式下的3维点
+    }
+    
+    static MapPoint::Ptr createMapPoint();
+    static MapPoint::Ptr createMapPoint( 
+        const Vector3d& pos_world, 
+        const Vector3d& norm_,
+        const Mat& descriptor,
+        Frame* frame );
+};
 }
 
-MapPoint::Ptr MapPoint::createMapPoint ( 
-    const Vector3d& pos_world, 
-    const Vector3d& norm, 
-    const Mat& descriptor, 
-    Frame* frame )
-{
-    return MapPoint::Ptr( 
-        new MapPoint( factory_id_++, pos_world, norm, frame, descriptor )
-    );
-}
-
-unsigned long MapPoint::factory_id_ = 0;
-
-}
+#endif // MAPPOINT_H
