1dc6ee2c61e3aa20b1fde4d4d624c64c25de9b88
Wan Youwen <1756429307@qq.com>
2018-01-29 15:58:19 +0800

    地图类

--- vSLAM/oRB_SLAM2/src/Map.cc
+++ vSLAM/oRB_SLAM2/src/Map.cc
@@ -1,21 +1,7 @@
 /**
 * This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+* 地图 管理 关键帧 地图点
+*  添加/删除  关键帧/地图点
 */
 
 #include "Map.h"
@@ -24,7 +10,7 @@
 
 namespace ORB_SLAM2
 {
-
+// 初始 最大关键帧id = 0  最大变化id = 0
 Map::Map():mnMaxKFid(0),mnBigChangeIdx(0)
 {
 }
@@ -32,30 +18,41 @@ Map::Map():mnMaxKFid(0),mnBigChangeIdx(0)
 void Map::AddKeyFrame(KeyFrame *pKF)
 {
     unique_lock<mutex> lock(mMutexMap);
-    mspKeyFrames.insert(pKF);
-    if(pKF->mnId>mnMaxKFid)
-        mnMaxKFid=pKF->mnId;
+    mspKeyFrames.insert(pKF);// set 集合 插入关键帧
+    if(pKF->mnId > mnMaxKFid)// 关键帧 id
+        mnMaxKFid=pKF->mnId;// 最大关键帧 的id
 }
 
 void Map::AddMapPoint(MapPoint *pMP)
 {
     unique_lock<mutex> lock(mMutexMap);
-    mspMapPoints.insert(pMP);
+    mspMapPoints.insert(pMP);// set 集合 插入 地图点
 }
 
 void Map::EraseMapPoint(MapPoint *pMP)
 {
     unique_lock<mutex> lock(mMutexMap);
-    mspMapPoints.erase(pMP);
+    
+    // wyw 添加
+  //      auto  it = mspMapPoints.find(pMP);// 获取指针
+  //      delete *it;//删除
+    
+    mspMapPoints.erase(pMP);// 删除了指针  内容清除？
 
     // TODO: This only erase the pointer.
     // Delete the MapPoint
+
 }
 
 void Map::EraseKeyFrame(KeyFrame *pKF)
 {
     unique_lock<mutex> lock(mMutexMap);
-    mspKeyFrames.erase(pKF);
+    
+        // wyw 添加
+   //     auto  it = mspKeyFrames.find(pKF);// 获取指针
+   //     delete *it;//删除
+        
+    mspKeyFrames.erase(pKF);// 删除了指针  内容清除？
 
     // TODO: This only erase the pointer.
     // Delete the MapPoint
@@ -70,7 +67,7 @@ void Map::SetReferenceMapPoints(const vector<MapPoint *> &vpMPs)
 void Map::InformNewBigChange()
 {
     unique_lock<mutex> lock(mMutexMap);
-    mnBigChangeIdx++;
+    mnBigChangeIdx++;// id++
 }
 
 int Map::GetLastBigChangeIdx()
@@ -79,36 +76,39 @@ int Map::GetLastBigChangeIdx()
     return mnBigChangeIdx;
 }
 
+// 返回所有关键帧
 vector<KeyFrame*> Map::GetAllKeyFrames()
 {
     unique_lock<mutex> lock(mMutexMap);
     return vector<KeyFrame*>(mspKeyFrames.begin(),mspKeyFrames.end());
 }
 
+// 返回所有地图点
 vector<MapPoint*> Map::GetAllMapPoints()
 {
     unique_lock<mutex> lock(mMutexMap);
     return vector<MapPoint*>(mspMapPoints.begin(),mspMapPoints.end());
 }
 
+// 地图中地图点的数量
 long unsigned int Map::MapPointsInMap()
 {
     unique_lock<mutex> lock(mMutexMap);
     return mspMapPoints.size();
 }
-
+// 地图中 关键帧数量
 long unsigned int Map::KeyFramesInMap()
 {
     unique_lock<mutex> lock(mMutexMap);
     return mspKeyFrames.size();
 }
-
+// 地图中 参考地图点
 vector<MapPoint*> Map::GetReferenceMapPoints()
 {
     unique_lock<mutex> lock(mMutexMap);
     return mvpReferenceMapPoints;
 }
-
+// 最大 id的 关键帧 的 id
 long unsigned int Map::GetMaxKFid()
 {
     unique_lock<mutex> lock(mMutexMap);
@@ -118,12 +118,12 @@ long unsigned int Map::GetMaxKFid()
 void Map::clear()
 {
     for(set<MapPoint*>::iterator sit=mspMapPoints.begin(), send=mspMapPoints.end(); sit!=send; sit++)
-        delete *sit;
+        delete *sit;// 删除地图点内容
 
     for(set<KeyFrame*>::iterator sit=mspKeyFrames.begin(), send=mspKeyFrames.end(); sit!=send; sit++)
-        delete *sit;
+        delete *sit;// 删除 关键帧
 
-    mspMapPoints.clear();
+    mspMapPoints.clear();//清楚全部
     mspKeyFrames.clear();
     mnMaxKFid = 0;
     mvpReferenceMapPoints.clear();
