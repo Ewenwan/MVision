dbca4b681013be30638f0e91242d2271bae5bde3
Wan Youwen <1756429307@qq.com>
2018-03-22 13:32:10 +0800

    根据点云索引提取对应的点云

---
+++ PCL_APP/Basic/Filtering/extract_indices.cpp
@@ -0,0 +1,42 @@
+/*
+ 提取 点云索引
+ 根据点云索引提取对应的点云
+ */
+
+// STL
+#include <iostream>
+
+// PCL
+#include <pcl/point_types.h>
+#include <pcl/filters/extract_indices.h>
+
+int
+main (int, char**)
+{
+  typedef pcl::PointXYZ PointType;
+  typedef pcl::PointCloud<PointType> CloudType;
+  CloudType::Ptr cloud (new CloudType);
+  cloud->is_dense = false;
+  // 生成点云 
+  PointType p;
+  for (unsigned int i = 0; i < 5; ++i)
+  {
+    p.x = p.y = p.z = static_cast<float> (i);
+    cloud->push_back (p);
+  }
+
+  std::cout << "Cloud has " << cloud->points.size () << " points." << std::endl;
+
+  pcl::PointIndices indices;// 取得需要的索引
+  indices.indices.push_back (0);
+  indices.indices.push_back (2);
+
+  pcl::ExtractIndices<PointType> extract_indices;//索引提取器
+  extract_indices.setIndices (boost::make_shared<const pcl::PointIndices> (indices));//设置索引
+  extract_indices.setInputCloud (cloud);//设置输入点云
+  pcl::PointCloud<pcl::PointXYZ>::Ptr output (new pcl::PointCloud<pcl::PointXYZ>);
+  extract_indices.filter (*output);//提取对于索引的点云
+
+  std::cout << "Output has " << output->points.size () << " points." << std::endl;
+  return (0);
+}
