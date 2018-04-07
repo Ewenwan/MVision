c44a76b22361b22d723afc39774ea52a771be0b3
Wan Youwen <1756429307@qq.com>
2018-04-07 16:29:29 +0800

    无序点云数据集的空间变化检测

--- PCL_APP/Basic/Ttee/octree_change_detection.cpp
+++ PCL_APP/Basic/Ttee/octree_change_detection.cpp
@@ -1,5 +1,4 @@
 /*
-
 无序点云数据集的空间变化检测
  
 pcl::octree::OctreePointCloudChangeDetector
@@ -25,7 +24,7 @@ octree是一种用于管理稀疏3D数据的树状数据结构，
 int
 main (int argc, char** argv)
 {
-  srand ((unsigned int) time (NULL));//随机数种子
+  srand ((unsigned int) time (NULL));//用系统时间初始化随机种子
 
   //=====【1】八叉树分辨率即体素的大小===========
   float resolution = 32.0f;
@@ -33,14 +32,14 @@ main (int argc, char** argv)
   //=====【2】初始化空间变化检测对象===============
   pcl::octree::OctreePointCloudChangeDetector<pcl::PointXYZ> octree (resolution);
 
-  // ======【3】新建一个点云 cloudA
+  // ======【3】新建一个点云 cloudA  创建点云实例cloudA生成的点云数据用于建立八叉树octree对象
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloudA_ptr (new pcl::PointCloud<pcl::PointXYZ> );
   pcl::PointCloud<pcl::PointXYZ>& cloudA = *cloudA_ptr;
   // 为点云 cloudA 产生数据  所生成的点数据用于建立八叉树octree对象。
   cloudA.width = 128;
-  cloudA.height = 1;
-  cloudA.points.resize (cloudA.width * cloudA.height);
-  for (size_t i = 0; i < cloudA.points.size (); ++i)
+  cloudA.height = 1;//无序点
+  cloudA.points.resize (cloudA.width * cloudA.height);//总数
+  for (size_t i = 0; i < cloudA.points.size (); ++i)//循环填充
   {
     cloudA.points[i].x = 64.0f * rand () / (RAND_MAX + 1.0f);
     cloudA.points[i].y = 64.0f * rand () / (RAND_MAX + 1.0f);
@@ -51,13 +50,12 @@ main (int argc, char** argv)
   octree.setInputCloud (cloudA_ptr);//设置输入点云
   octree.addPointsFromInputCloud ();//从输入点云构建八叉树
 
-// 点云cloudA是参考点云，用其建立的八叉树对象描述它的空间分布
-//OctreePointCloudChangeDetector类继承自Octree2BufBase类，
-//Octree2BufBase类允许同时在内存中保存和管理两个octree，
-//另外，它应用了内存池，该机制能够重新利用已经分配了的节点对象，
-//因此减少了在生成多个点云八叉树对象时昂贵的内存分配和释放操作。
-//通过访问“octree.switchBuffers()”，
-//重置了八叉树octree对象的缓冲区，但把之前的octree数据仍保留在内存中。
+ /***********************************************************************************
+    点云cloudA是参考点云用其建立的八叉树对象描述它的空间分布，octreePointCloudChangeDetector
+    类继承自Octree2BufBae类，Octree2BufBae类允许同时在内存中保存和管理两个octree，另外它应用了内存池
+    该机制能够重新利用已经分配了的节点对象，因此减少了在生成点云八叉树对象时昂贵的内存分配和释放操作
+    通过访问 octree.switchBuffers ()重置八叉树 octree对象的缓冲区，但把之前的octree数据仍然保留在内存中
+   ************************************************************************************/
   octree.switchBuffers ();// 交换八叉树缓存，但是cloudA对应的八叉树结构仍在内存中
 
 // ======【5】新建另一个点云 cloudB=====================
@@ -77,13 +75,13 @@ main (int argc, char** argv)
   //添加 cloudB到八叉树
   octree.setInputCloud (cloudB_ptr);
   octree.addPointsFromInputCloud ();
-/*
+/**************************************
 为了检索到获取存在于cloudB的点集R，此R并没有cloudA中元素，   B - B交A
 可以调用getPointIndicesFromNewVoxels方法，通过探测两个八叉树之间体素的不同，
 它返回cloudB中新加点的索引的向量，通过索引向量可以获取R点集。
 很明显，这样就探测了cloudB相对于cloudA变化的点集，
 但是只能探测在cloudA上增加的点集，而不能探测在cloudA上减少的点集。
-*/
+**************************************/
 
   std::vector<int> newPointIdxVector; //存储新加点的索引的向量
   //获取前一cloudA对应的八叉树 在 cloudB对应八叉树中 A内没有的点集
