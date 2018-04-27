a309195b28e7bcbf2ddfba795c9a3b16ecf65a03
Wan Youwen <1756429307@qq.com>
2018-04-27 09:06:14 +0800

    残差网络 f(x) + W*x

--- CNN/ResNet50.py
+++ CNN/ResNet50.py
@@ -12,7 +12,7 @@ ResNet50
 5  3个  3*3卷积核 512输出的残差模块 2048/4 = 512  且第一个残差块的第一个卷积步长为2  
 6  均值池化 
 7  全连接层 输出 1000  类
-8  softmax 类别预测
+8  softmax分类 预测类别输出
 
 实际中，考虑计算的成本，对残差块做了计算优化，即将2个3x3的卷积层替换为 1x1 + 3x3 + 1x1 。
 新结构中的中间3x3的卷积层首先在一个降维1x1卷积层下减少了计算，然后在另一个1x1的卷积层下做了还原，既保持了精度又减少了计算量。
@@ -102,26 +102,27 @@ class ResNet50(object):
 
         with tf.variable_scope(scope):
             # 定义模型结构 construct the model
-            # 64个输出  7*7卷积核 步长 2 224*224图像输入 大小减半
+            ###### 1.  64个输出  7*7卷积核 步长 2 224*224图像输入 大小减半#########
             net = conv2d(inputs, 64, 7, 2, scope="conv1") # -> [batch, 112, 112, 64]
-            # 先 批规范化 在 relu激活
+            ####### 先 批规范化 在 relu激活
             net = tf.nn.relu(batch_norm(net, is_training=self.is_training, scope="bn1"))
-            # 最大值池化 步长2 大小减半
+            ##### 最大值池化 步长2 大小减半
             net = max_pool(net, 3, 2, scope="maxpool1")  # -> [batch, 56, 56, 64]
-            # 3个 3*3卷积核 64输出的残差模块 256/4 = 64  且第一个残差块的第一个卷积步长为1
+			
+            ##### 2.  3个 3*3卷积核 64输出的残差模块 256/4 = 64  且第一个残差块的第一个卷积步长为1
             net = self._block(net, 256, 3, init_stride=1, is_training=self.is_training,scope="block2") # -> [batch, 56, 56, 256]
-            # 4个 3*3卷积核 128输出的残差模块 512/4 = 128 且第一个残差块的第一个卷积步长为2             
+            ##### 3.  4个 3*3卷积核 128输出的残差模块 512/4 = 128 且第一个残差块的第一个卷积步长为2             
             net = self._block(net, 512, 4, is_training=self.is_training, scope="block3")# -> [batch, 28, 28, 512]
-            # 6个 3*3卷积核 256输出的残差模块 1024/4 = 256且第一个残差块的第一个卷积2                                                    
+            ##### 4.  6个 3*3卷积核 256输出的残差模块 1024/4 = 256且第一个残差块的第一个卷积2                                                    
             net = self._block(net, 1024, 6, is_training=self.is_training, scope="block4")# -> [batch, 14, 14, 1024]
-            # 3个 3*3卷积核 512输出的残差模块 2048/4 = 512 且第一个残差块的第一个卷积步长为2                                              
+            ##### 5.  3个 3*3卷积核 512输出的残差模块 2048/4 = 512 且第一个残差块的第一个卷积步长为2                                              
             net = self._block(net, 2048, 3, is_training=self.is_training, scope="block5")# -> [batch, 7, 7, 2048]
-            # 均值池化                                          
+            ##### 6.  均值池化                                          
             net = avg_pool(net, 7, scope="avgpool5") # -> [batch, 1, 1, 2048]  
             net = tf.squeeze(net, [1, 2], name="SpatialSqueeze") # -> [batch, 2048]
-            # 全连接层
+            ##### 7. 全连接层
             self.logits = fc(net, self.num_classes, "fc6")       # -> [batch, num_classes]
-            # 预测输出
+            ##### 8. softmax分类 预测输出
             self.predictions = tf.nn.softmax(self.logits)
 
     # 残差块集合 默认卷积步长为2
