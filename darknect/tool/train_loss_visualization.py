3924447ac045f8ad205a0483bcdab2177bf4b454
Wan Youwen <1756429307@qq.com>
2018-05-07 17:19:40 +0800

    训练数据 损失可视化

---
+++ darknect/tool/train_loss_visualization.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+lines =1878760
+result = pd.read_csv('S:/Tools/Paul_YOLO/paul_train_log_new.txt', skiprows=[x for x in range(lines) if ((x%10!=9) |(x<1000))] ,error_bad_lines=False, names=['loss', 'avg', 'rate', 'seconds', 'images'])
+result.head()
+
+result['loss']=result['loss'].str.split(' ').str.get(1)
+result['avg']=result['avg'].str.split(' ').str.get(1)
+result['rate']=result['rate'].str.split(' ').str.get(1)
+result['seconds']=result['seconds'].str.split(' ').str.get(1)
+result['images']=result['images'].str.split(' ').str.get(1)
+result.head()
+result.tail()
+
+#print(result.head())
+# print(result.tail())
+# print(result.dtypes)
+
+print(result['loss'])
+print(result['avg'])
+print(result['rate'])
+print(result['seconds'])
+print(result['images'])
+
+result['loss']=pd.to_numeric(result['loss'])
+result['avg']=pd.to_numeric(result['avg'])
+result['rate']=pd.to_numeric(result['rate'])
+result['seconds']=pd.to_numeric(result['seconds'])
+result['images']=pd.to_numeric(result['images'])
+result.dtypes
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(result['avg'].values,label='avg_loss')
+#ax.plot(result['loss'].values,label='loss')
+ax.legend(loc='best')
+ax.set_title('The loss curves')
+ax.set_xlabel('batches')
+fig.savefig('avg_loss')
+#fig.savefig('loss')
