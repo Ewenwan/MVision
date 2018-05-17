bc6ce8a6c88d824c422c8d34cdb2858d89f3b0cb
Wan Youwen <1756429307@qq.com>
2018-05-17 16:11:00 +0800

    Update train.sh

--- darknect/caffe/train_test_val/train.sh
+++ darknect/caffe/train_test_val/train.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
-# train
 # caffe main path
 CAFFE_HOME=../..
 # learning rate paramter
 SOLVER=./gnet_solver.prototxt
 # init weights 
-WEIGHTS=/your/path/to/bvlc_googlenet.caffemodel
+WEIGHTS=/home/wanyouwen/ewenwan/software/caffe-yolo/weights/bvlc_googlenet.caffemodel
 # training  --gpu=0,1,2,3,4,5,6,7
 $CAFFE_HOME/build/tools/caffe train \
-    --solver=$SOLVER --weights=$WEIGHTS --gpu=0,1
+    --solver=$SOLVER --weights=$WEIGHTS \
+	--gpu=0,1,2,3 2>&1 | tee train_yolov1.log
