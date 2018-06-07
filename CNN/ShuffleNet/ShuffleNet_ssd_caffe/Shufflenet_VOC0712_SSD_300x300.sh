3f16a7112f8a35c5c04924c71e58246bd7fd9c3b
Wan Youwen <1756429307@qq.com>
2018-06-07 10:19:04 +0800

    Update Shufflenet_VOC0712_SSD_300x300.sh

--- CNN/ShuffleNet/ShuffleNet_ssd_caffe/Shufflenet_VOC0712_SSD_300x300.sh
+++ CNN/ShuffleNet/ShuffleNet_ssd_caffe/Shufflenet_VOC0712_SSD_300x300.sh
@@ -5,7 +5,8 @@ SOLVER="./SHN_ssd_33_solver.prototxt"
 
 #WEIGHTS="/home/wanyouwen/ewenwan/software/caffe_yolo/weights/VGG_ILSVRC_16_layers_fc_reduced.caffemodel"
 #WEIGHTS="./models/SHN_ssd_33_iter_19000.caffemodel"
-WEIGHTS="./models/SHN_ssd_33_iter_3000.caffemodel"
+#WEIGHTS="./models/SHN_ssd_33_iter_3000.caffemodel"
+WEIGHTS="./shufflenet_1x_g3.caffemodel"
 $CAFFE_HOME/build/tools/caffe train \
         --solver=$SOLVER --weights=$WEIGHTS \
-        --gpu=4,5,6,7 2>&1 | tee -a ./VGG_VOC0712_SSD_300x300.log
+        --gpu=1 2>&1 | tee -a ./VGG_VOC0712_SSD_300x300.log
