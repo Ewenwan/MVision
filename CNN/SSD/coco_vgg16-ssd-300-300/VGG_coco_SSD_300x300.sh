6e2392cbec6f4460bd98ca3fbaaef71e24490757
Wan Youwen <1756429307@qq.com>
2018-07-06 15:27:00 +0800

    Update VGG_coco_SSD_300x300.sh

--- CNN/SSD/coco_vgg16-ssd-300-300/VGG_coco_SSD_300x300.sh
+++ CNN/SSD/coco_vgg16-ssd-300-300/VGG_coco_SSD_300x300.sh
@@ -1,10 +1,10 @@
 # cd /home/wanyouwen/ewenwan/software/caffe-ssd/examples/ssd/SSD_300x300
 
 CAFFE_HOME=../../..
-SOLVER = "models/VGG_coco_SSD_300x300_solver.prototxt"
+SOLVER="./VGG_coco_SSD_300x300_solver.prototxt"
+#WEIGHTS="../SSD_300x300/models/ssd_33_iter_24000.caffemodel"
 WEIGHTS="/home/wanyouwen/ewenwan/software/caffe_yolo/weights/VGG_ILSVRC_16_layers_fc_reduced.caffemodel"
-
 $CAFFE_HOME/build/tools/caffe train \
---solver= $SOLVER \
---weights= WEIGHTS \
---gpu 0,1,2,3 2>&1 | tee -a ./VGG_coco_SSD_300x300.log
+      --solver=$SOLVER \
+      --weights=$WEIGHTS \
+      --gpu=4,5 2>&1 | tee -a ./VGG_coco_SSD_300x300.log
