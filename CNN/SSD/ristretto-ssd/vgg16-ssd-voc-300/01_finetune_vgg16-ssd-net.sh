e3fb77391adae8ab802bd18f058af63ac1395359
Wan Youwen <1756429307@qq.com>
2018-07-16 14:28:31 +0800

    Create 01_finetune_vgg16-ssd-net.sh

---
+++ CNN/SSD/ristretto-ssd/vgg16-ssd-voc-300/01_finetune_vgg16-ssd-net.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# finetune 微调
+
+SOLVER="../../../examples/ssd/ssd-voc-300/solver_finetune.prototxt"
+WEIGHTS="../../../models/ssd-voc-300/ssd_voc_300.caffemodel"
+
+../../../build/tools/caffe train \
+    --solver=$SOLVER \
+    --weights=$WEIGHTS \
+    --gpu=4 2>&1 | tee -a ./ssd_voc_300_quantized_finetune.log
