5cd6c42abdff79e17519c06026f17bbc92e6a649
Wan Youwen <1756429307@qq.com>
2018-07-16 14:27:55 +0800

    Update 00_quantize_ssdnet_voc.sh

--- CNN/SSD/ristretto-ssd/vgg16-ssd-voc-300/00_quantize_ssdnet_voc.sh
+++ CNN/SSD/ristretto-ssd/vgg16-ssd-voc-300/00_quantize_ssdnet_voc.sh
@@ -3,8 +3,8 @@
 
 ../../../build/tools/ristretto quantize \
         --model=./ssd_33_voc_train_val.prototxt \
-        --weights=../src/models/ssd_33_iter_9000.caffemodel \
+        --weights=../src/models/ssd_33_iter_800.caffemodel \
         --model_quantized=./ssd_33_voc_quantized.prototxt \
-        --trimming_mode=dynamic_fixed_point --gpu=7 --iterations=2000 \
-        --error_margin=3
-
+        --trimming_mode=dynamic_fixed_point --gpu=4 --iterations=2000 \
+        --error_margin=3 \
+        --net_type=ssd_detection
