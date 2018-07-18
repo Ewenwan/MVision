9366a24922973233f6fed0cb1dde80815513a7d3
Wan Youwen <1756429307@qq.com>
2018-07-18 11:30:31 +0800

    Create 02_test_quantize_vgg16-ssd-net-voc.sh

---
+++ CNN/SSD/ristretto-ssd/vgg16-ssd-voc-300/02_test_quantize_vgg16-ssd-net-voc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# finetune 微调
+
+MODELS="../../../models/ssd-voc-300/ssd_voc_300_quantized.prototxt"
+WEIGHTS="../../../models/ssd-voc-300/ssd-voc-300-quantized-weights_iter_2000.caffemodel"
+
+../../../build/tools/caffe test \
+    --model=$MODELS \
+    --weights=$WEIGHTS \
+    --iterations=2000 \
+    --net_type=ssd_detection \
+    --gpu=6 2>&1 | tee -a ./ssd_voc_300_quantized_test.log
